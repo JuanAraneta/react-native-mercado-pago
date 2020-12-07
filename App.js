ca789ce9a430ca97c982db233d3cc2e3e3e91800
Juan Araneta <Juanaraneta10@gmail.com>
2020-12-06 21:07:17 -0300

    Follow library installation steps

--- App.js
+++ App.js
@@ -1,114 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import Env from 'react-native-config';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Alert} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import MercadoPagoCheckout from '@blackbox-vision/react-native-mercadopago-px';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import styles from './styles';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+// You should create the preference server-side, not client-side but we show client-side for the sake of simplicity
+const getPreferenceId = async (payer, ...items) => {
+  const response = await fetch(
+    `https://api.mercadopago.com/checkout/preferences?access_token=${Env.MP_ACCESS_TOKEN}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        items,
+        payer: {
+          email: payer,
+        },
+      }),
+    },
   );
+
+  const preference = await response.json();
+
+  return preference.id;
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+export default function App() {
+  const [paymentResult, setPaymentResult] = useState(null);
 
-export default App;
+  const startCheckout = async () => {
+    try {
+      const preferenceId = await getPreferenceId('payer@email.com', {
+        title: 'Dummy Item Title',
+        description: 'Dummy Item Description',
+        quantity: 1,
+        currency_id: 'ARS',
+        unit_price: 10.0,
+      });
+
+      const payment = await MercadoPagoCheckout.createPayment({
+        publicKey: Env.MP_PUBLIC_KEY,
+        preferenceId,
+      });
+
+      setPaymentResult(payment);
+    } catch (err) {
+      Alert.alert('Something went wrong', err.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={startCheckout}>
+        <Text style={styles.text}>Start Payment</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Payment: {JSON.stringify(paymentResult)}</Text>
+    </View>
+  );
+}
